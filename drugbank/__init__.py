67d211c17991f48122a5c2c4b436fed0bac013f9
czjczj <2838588360@qq.com>
2021-05-18 09:16:28 +0800

    init

---
+++ drugbank/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time  : 2021/05/13 10:48
+#@Author: czj
+#@Email : zj_cheng@csu.edu.cn
+#@File  : __init__.py.py
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+mpl.rcParams['font.family'] = 'sans-serif'
